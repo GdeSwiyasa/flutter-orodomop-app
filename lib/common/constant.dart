ccd6d40a2c0e082449e66af28a908095eea9f187
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-05-24 12:16:41 +0800

    add constant for guide we develop app

---
+++ lib/common/constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//COLOR
+const Color kPrimaryColor = Color(0xffCD3599);
+const Color blackColor = Color(0xff121212);
+
+//TextStyle
+final TextStyle kHeading5 =
+    GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400);
+final TextStyle kHeading6 = GoogleFonts.poppins(
+    fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15);
+final TextStyle kSubtitle = GoogleFonts.poppins(
+    fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15);
+final TextStyle kBodyText = GoogleFonts.poppins(
+    fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.25);
