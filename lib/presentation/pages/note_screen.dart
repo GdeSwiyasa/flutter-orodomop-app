4290e8109a9b660a76f75e32a6fd18761b38df39
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-05-25 11:52:39 +0800

    add some widget to note screen

--- lib/presentation/pages/note_screen.dart
+++ lib/presentation/pages/note_screen.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:orodomop_app/common/constant.dart';
 
 class NoteScreen extends StatelessWidget {
   static const route = '/note_screen';
-  const NoteScreen({Key? key}) : super(key: key);
 
+  const NoteScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Note Screen"),
+        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.white),
         centerTitle: true,
+        title: const Text('NOTE'),
+        backgroundColor: whiteColor,
+        foregroundColor: blackColor,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text("Note Screen"),
+      body: Container(
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            return Card(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(15)),
+                padding:
+                    EdgeInsets.only(top: 8, right: 24, left: 24, bottom: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Jadwal mata pelajaran'),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: 8,
+          padding: EdgeInsets.all(16),
+          separatorBuilder: (context, index) => Divider(height: 16),
+        ),
       ),
     );
   }
